c762fda4522ce7a1af86d699b773f40f8ef31402
WeiyiWu <wwu3@ualberta.ca>
2021-09-03 20:48:34 +0800

    update

--- lab1.py
+++ lab1.py
@@ -1,5 +1,7 @@
 import requests
+import os
 print(requests.__version__)
 print(requests.get('https://www.google.com/'))
-#import os
 #os.system("curl -i http://www.google.com/")
+os.system('curl -o lab1.py https://raw.githubusercontent.com/WeiyiWu/CMPUT404Lab1/main/lab1.py')
+os.system('curl https://raw.githubusercontent.com/WeiyiWu/CMPUT404Lab1/main/lab1.py')
