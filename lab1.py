c928b5a3dfd11b6c49e9096077c6e08d4db441ed
WeiyiWu <wwu3@ualberta.ca>
2021-09-10 21:02:19 +0800

    update

--- lab1.py
+++ lab1.py
@@ -6,10 +6,8 @@ print(requests.__version__)
 print(requests.get('https://www.google.com/'))
 
 # download python script
-# resource: https://www.tutorialspoint.com/downloading-files-from-web-using-python
 rawURL = 'https://raw.githubusercontent.com/WeiyiWu/CMPUT404Lab1/main/lab1.py'
 r = requests.get(rawURL, allow_redirects=True)
-open('lab01.py', 'wb').write(r.content)
 # print source code
 print(r.text)
 
